5321b88fc2274dcf1b61b1e4fc3d1d8ee784e85d
Artem pkeeper Melanich <pensivekeeper@gmail.com>
2018-03-23 16:12:34 +0200

    proper package format for pypi

---
+++ huepy/__init__.py
@@ -0,0 +1,8 @@
+from huepy.hue import *
+
+
+__all__ = ['bad', 'bg', 'black', 'blue', 'bold', 'cyan', 'good', 'green',
+           'grey', 'info', 'italic', 'lightblue', 'lightcyan', 'lightgreen',
+           'lightpurple', 'lightred', 'orange', 'purple', 'que', 'red', 'run',
+           'strike', 'under', 'white', 'yellow']
+__version__ = '1.0.0'
