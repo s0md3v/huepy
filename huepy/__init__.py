d29cbed2ccd9e4b7603dd3145de777bd4a65dca7
Somdev Sangwan <s0md3v@gmail.com>
2019-12-12 12:03:50 +0530

    Delete __init__.py

--- huepy/__init__.py
+++
@@ -1,8 +0,0 @@
-from .hue import COMMANDS
-
-__all__ = list(COMMANDS.keys())
-__version__ = '1.0.1'
-
-if __name__ == '__main__':
-    print('All commands: ', __all__)
-    print('Current version: ', __version__)
