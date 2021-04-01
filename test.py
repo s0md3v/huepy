fa18f2a3788cc7aef977bd8d8f13ec7f415eeaa4
Devalla <devallabharath619@gmail.com>
2021-04-02 00:37:05 +0530

    Added test code to the Repo (test.py)
    The test code contains the preview and the help info.

---
+++ test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from huepy import *
+
+
+def Preview():
+    print("")
+    print(bold("    AVAILABLE COLORS    ")+bold("    AVAILABLE STYLES    ")+bold("    AVAILABLE MODES"))
+    print("")
+    print(white("  ████████████████████  ")+bold("       Bold Text             ")+info(under("Info")+ " Sign!"))
+    print(grey("  ████████████████████  ")+"       "+bg("BackGround")+"            "+que(under("Que")+ " Sign!"))
+    print(black("  ████████████████████  ")+"       "+under("Under Line")+"            "+run(under("Run")+ " Sign!"))
+    print(green("  ████████████████████  ")+"     "+strike("Strike Through")+"          "+bad(under("Bad")+ " Sign!"))
+    print(lightgreen("  ████████████████████  ")+italic("      Italic Text            ")+good(under("Good")+ " Sign!"))
+    print(cyan("  ████████████████████       ")+bold(under("Bold Underline")))
+    print(lightcyan("  ████████████████████        ")+bold(strike("Bold Strike")))
+    print(red("  ████████████████████        ")+bold(italic("Bold Italic")))
+    print(blue("  ████████████████████"))
+    print(lightblue("  ████████████████████"))
+    print(purple("  ████████████████████"))
+    print(lightpurple("  ████████████████████"))
+    print(orange("  ████████████████████"))
+    print(yellow("  ████████████████████"))
+
+
+def Help():
+    Preview()
+    print("""
+    Colors :  white, grey, black, green, lightgreen, cyan, lightcyan,
+              red, blue, lightblue, purple, lightpurple, orange, yellow
+
+        print(red("This is Red Colored Text"))
+
+    Styles :  bold, bg, under, strike, italic
+
+        print(bold("Some Text"))
+        print(bold(italic("Some more Text")))
+
+    Modes  :  info, que, run, bad, good
+
+        print(bad("An error occured!"))
+    """)
+
+
+if __name__ == "__main__":
+    Help()
